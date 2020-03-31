dbbbea2f81cbbfc97a2f1475993a9a855fa2c493
ionutpantazi <ionut28p@yahoo.com>
2020-03-31 19:31:22 +0100

    update componente

--- src/componente/Authenticated.js
+++ src/componente/Authenticated.js
@@ -1,86 +1,49 @@
 import React from "react";
+import Mesaj from "./Mesaj";
 import firebase from "firebase/app";
-import { 
+import {
   FirebaseDatabaseProvider,
   FirebaseDatabaseNode,
-  FirebaseDatabaseMutation
 } from "@react-firebase/database";
 import { config } from "../config";
 
-import { Form, Input, Button, Row, Col, Comment } from 'antd';
+import { Button, Row, Col, Comment } from 'antd';
 import { createFromIconfontCN } from '@ant-design/icons';
 import moment from 'moment';
 
 class Authenticated extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: ''
-    };
-  }
 
-  onChange = ({ target: { value } }) => {
-    this.setState({ value });
-  };
   render() {
-    const { TextArea } = Input;
     const IconFont = createFromIconfontCN({
       scriptUrl: '//at.alicdn.com/t/font_1697557_jkfmpzlqesp.js',
     });
     return (
       <div style={{ margin: '20px' }}>
         <FirebaseDatabaseProvider firebase={firebase} {...config}>
-          <div>
-            <Row justify="space-around" align="middle">
-              <Col span={12} >
-                <b>Bine ai venit {this.props.user}</b><br />
-              </Col>
-              <Col span={12}>
-                <Button style={{ float: 'right' }} type='primary' onClick={() => { firebase.auth().signOut() }}>Sign out</Button>
-              </Col>
-            </Row>
-            <FirebaseDatabaseNode path="mesaje/">
-              {data => {
-                const { value } = data;
-                if (value === null || typeof value === "undefined") return null;
-                const values = Object.values(value);
-                return values.map((val) => (
-                  <Comment
-                    avatar={<IconFont type="icon-avatar_user" style={{ fontSize: '30px' }} />}
-                    author={<b>{val.nume}</b>}
-                    content={<i>{val.mesaj}</i>}
-                    datetime={<span>{moment([val.data], "LLLL").fromNow()}</span>}
-                  />
-                ));
-              }}
-            </FirebaseDatabaseNode>
-          </div>
-          <div>
-            <FirebaseDatabaseMutation type="push" path="mesaje/">
-              {({ runMutation }) => {
-                return (
-                  <div>
-                    <Form onFinish={this.onFinish}>
-                      <Form.Item
-                        placeholder="mesaj"
-                        onChange={this.onChange}
-                      >
-                        <TextArea allowClear autoSize />
-                      </Form.Item>
-                      <Form.Item>
-                        <Button
-                          type="primary"
-                          htmlType="submit"
-                          onClick={async () => {
-                            await runMutation({ nume: this.props.user, mesaj: this.state.value, data: moment(new Date()).format("LLLL") });
-                          }}>Trimite</Button>
-                      </Form.Item>
-                    </Form>
-                  </div>
-                )
-              }}
-            </FirebaseDatabaseMutation>
-          </div>
+          <Row justify="space-around" align="middle">
+            <Col span={12} >
+              <b>Bine ai venit {this.props.user}</b><br />
+            </Col>
+            <Col span={12}>
+              <Button style={{ float: 'right' }} type='primary' onClick={() => { firebase.auth().signOut() }}>Sign out</Button>
+            </Col>
+          </Row>
+          <FirebaseDatabaseNode path="mesaje/">
+            {data => {
+              const { value } = data;
+              if (value === null || typeof value === "undefined") return null;
+              const values = Object.values(value);
+              return values.map((val) => (
+                <Comment
+                  avatar={<IconFont type="icon-avatar_user" style={{ fontSize: '30px' }} />}
+                  author={<b>{val.nume}</b>}
+                  content={<i>{val.mesaj}</i>}
+                  datetime={<span>{moment([val.data], "LLLL").fromNow()}</span>}
+                />
+              ));
+            }}
+          </FirebaseDatabaseNode>
+          <Mesaj user={this.props.user} />
         </FirebaseDatabaseProvider>
       </div>
     )
---
+++ src/componente/Mesaj.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  FirebaseDatabaseMutation
+} from "@react-firebase/database";
+import { Form, Button, Input } from 'antd';
+import moment from 'moment';
+
+const Mesaj = (nume) => {
+  const [form] = Form.useForm();
+  const { TextArea } = Input;
+
+  return (
+    <FirebaseDatabaseMutation type="push" path="mesaje/">
+      {({ runMutation }) => {
+        return (
+          <Form form={form}>
+            <Form.Item
+              name="mesaj"
+            >
+              <TextArea autoSize={{ minRows: 1 }} />
+            </Form.Item>
+            <Form.Item shouldUpdate>
+              {() => (
+                <Button
+                  disabled={
+                    !form.isFieldsTouched(false) ||
+                    form.getFieldsError().filter(({ errors }) => errors.length).length
+                  }
+                  type="primary"
+                  htmlType="submit"
+                  onClick={async () => {
+                    const values = await form.validateFields();
+                    await runMutation({ nume: nume.user, mesaj: values.mesaj, data: moment(new Date()).format("LLLL") })
+                    form.resetFields();
+                  }}
+                >
+                  Trimite
+                </Button>
+              )}
+            </Form.Item>
+          </Form>
+        )
+      }}
+    </FirebaseDatabaseMutation>
+  );
+};
+
+export default Mesaj
